eeb2e430a42ef2f81a23018418a1ffec22b796d1
MariahAdams <mariah.adams23@yahoo.com>
2018-09-06 15:14:49 -0700

    wiring up goal item component

---
+++ src/components/goals/GoalItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class GoalItem extends Component {
+
+  render() { 
+
+    return (
+      <li>
+        <h4>GOAL ITEM</h4>
+      </li>
+    );
+  }
+}
+ 
+export default GoalItem;
--- src/components/goals/Goals.js
+++ src/components/goals/Goals.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import styles from './Goal.css';
+import PropTypes from 'prop-types';
+import GoalItem from './GoalItem';
+import styles from './Goals.css';
 
 class Goals extends Component {
+
+  static propTypes = {
+    goals: PropTypes.array
+  };
   
   render() { 
+    // const { goals } = this.props;
 
     return (
-      <section>
+      <section className={styles.goals}>
         <h2>Hello Goals!</h2>
+        <ul className="list">
+          {/* {goals.map(id => <GoalItem key={id} id={id}/>)} */}
+          <GoalItem/>
+        </ul>
       </section>
     );
   }
