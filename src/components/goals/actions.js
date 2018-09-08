6e51e45ee8aec92f65564a6099bbea8db634b0b6
MariahAdams <mariah.adams23@yahoo.com>
2018-09-08 15:56:12 -0700

    working on goal actions

--- src/components/goals/actions.js
+++ src/components/goals/actions.js
@@ -1,13 +1,27 @@
-import { GOALS_LOAD, getGoalList } from './reducers';
-import { getAllGoals } from '../../services/goalsApi';
+import { GOALS_LOAD, getGoalList, getGoalsById, GOAL_LOAD } from './reducers';
+import { getAllGoals, getGoal } from '../../services/goalsApi';
 
 export const loadGoals = () => (dispatch, getState) => {
-  const goalList = getGoalList(getState());
-  if(goalList.length) return;
-
+  const petList = getGoalList(getState());
+  if(petList.length) return;
+  
   dispatch({
     type: GOALS_LOAD,
     payload: getAllGoals()
   });
 };
 
+export const loadGoal = id => (dispatch, getState) => {
+  const goal = getGoalsById(getState(), id);
+  if(goal && goal.userId) return;
+
+  dispatch({
+    type: GOAL_LOAD,
+    payload: getGoal(id)
+  });
+};
+
+export const clearGoal = () => ({
+  type: GOAL_LOAD,
+  payload: null
+});
--- src/components/goals/actions.test.js
+++ src/components/goals/actions.test.js
@@ -1,12 +1,14 @@
 import { GOALS_LOAD } from './reducers';
 import { loadGoals } from './actions';
+import { getAllGoals } from '../../services/goalsApi';
 
 
 describe('goals action creators', () => {
   
 
   it('creates a load action', () => {
-    const { type } = loadGoals();
+    const { type, payload } = loadGoals();
     expect(type).toBe(GOALS_LOAD);
+    expect(payload).toBe(getAllGoals());
   });
 });
